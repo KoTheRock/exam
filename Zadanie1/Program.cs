fbe28fefc1de6d7787aaa6ec2bc74714bbd51232
KoTheRock <149530900+KoTheRock@users.noreply.github.com>
2024-01-01 23:09:34 +0300

    Add files via upload

---
+++ Zadanie1/Program.cs
@@ -0,0 +1,24 @@
+﻿public class Program
+{
+    static void Main()
+    {
+        string[] strings = {"Телевизор", "Холодильник","Рак", "Стол", "Привет", "Ком", "Чек"};
+        string[] array = new string[strings.Length];
+        int index = 0;
+
+        foreach (string word in strings)
+        {
+            if (word.Length <= 3)
+            {
+                array[index] = word;
+                index++;
+            }
+        }
+
+        string result = string.Join(" ", array, 0, index);
+        {
+            Console.WriteLine("Массив строк равный либо меньше 3: " + result);
+        }
+        
+    }
+}
